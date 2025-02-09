9d4d3a569e16e91779652079f839886b3b17ae49
Valentine Orga <valentineorga@gmail.com>
2025-02-09 07:25:00 +0100

    chore: get rid of default setup

--- packages/nextjs/components/providers/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/providers/ScaffoldEthAppWithProviders.tsx
@@ -6,10 +6,7 @@ import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowki
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { useTheme } from "next-themes";
-import { Toaster } from "react-hot-toast";
 import { WagmiProvider } from "wagmi";
-import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { UPProvider } from "~~/contexts/UPProviderContext";
 import { UniversalProfileProvider } from "~~/contexts/UniversalProfileContext";
@@ -23,14 +20,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang="en">
-      <body className={font.className}>
-        <div className={`flex flex-col min-h-screen `}>
-          <Header />
-          <main className="relative flex flex-col flex-1">{children}</main>
-          <Footer />
-        </div>
-        <Toaster />
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 };

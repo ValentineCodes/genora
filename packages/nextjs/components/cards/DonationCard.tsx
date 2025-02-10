756d97e95887b6927bd944747a4d8a236408cb56
Valentine Orga <valentineorga@gmail.com>
2025-02-10 10:34:12 +0100

    refactor: only show proposer if footer is hidden

--- packages/nextjs/components/cards/DonationCard.tsx
+++ packages/nextjs/components/cards/DonationCard.tsx
@@ -12,14 +12,16 @@ type Props = {
 
 export default function DonationCard({ title, description, timestamp, total, hideFooter }: Props) {
   return (
-    <div className="flex flex-col border border-black rounded-lg w-[250px] aspect-[3/2]">
+    <div className="flex flex-col border border-black rounded-lg w-[250px] aspect-[3/2] relative">
       <div className="flex flex-col flex-1 justify-center items-center text-center">
         <h1 className="text-lg font-semibold max-w-[75%]">{title}</h1>
         <text className="text-xs max-w-[75%]">{description}</text>
       </div>
 
-      {!hideFooter && (
-        <div className="px-4 py-2 flex justify-between items-center rounded-b-lg">
+      {hideFooter ? (
+        <div className="w-5 aspect-square border border-black rounded-full absolute bottom-2 right-2"></div>
+      ) : (
+        <div className="p-2 flex justify-between items-center rounded-b-lg">
           <div>
             <text className="text-xs">Sept 4, 2025</text>
           </div>
